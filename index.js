54a5eb9619f22a15dbfd17d0f3aa248239acdcc5
guoshiwei <guoshiwei@iMac-2.local>
2017-06-13 17:00:33 +0800

    gsw

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 // https://www.zhihu.com/topic/19668741/newest
 import { redis, es, config, mongo, filmMongo } from "./_base"
 import { split, pick } from "lodash"
-import { heartbeat } from "./zhihu/heartbeat"
+// import { heartbeat } from "./zhihu/heartbeat"
 import * as Epona from "eponajs"
+var schedule = require('node-schedule');
 global.btmsg = 'normal'
-heartbeat()
+    // heartbeat()
 
 function minutes(n) {
     return 1000 * 60 * n
 }
 
 function crawlAllCompleted(name) {
-    console.log("爬完")
-    setTimeout(run, minutes(3), name)
+    console.log("爬完一次，等待一个小时再次尝试")
+    setTimeout(run, minutes(60), name)
 }
-let lasttest = new Date(new Date - minutes(5))
+let lasttest = new Date(new Date - minutes(30))
 async function isblocked() {
-    if (new Date - lasttest < minutes(3)) { return false }
+    if (new Date - lasttest < minutes(30)) { return false }
     lasttest = new Date
-    let title = await Epona.get("https://movie.douban.com/subject/26850627", 'body > a')
-    if (title == "人工智能") {
+    let title = await Epona.get("https://movie.douban.com/subject/26850627", '#db-nav-movie > div.nav-wrap > div > div.nav-logo > a')
+    if (title == "豆瓣电影") {
         global.btmsg = 'normal'
         return false
     } else {
         global.btmsg = 'blocked'
-        console.log('ip blocked, retry after 5 mins')
+        console.log('ip blocked, retry after 30 mins')
         return true
     }
 }
@@ -34,14 +35,14 @@ function error(err) {
 }
 
 async function dispatch(crawler) {
-    // if (await isblocked()) {
-    //     setTimeout(dispatch, 1000 * 60 * 5, crawler)
-    //     return
-    // }
-    //父数据库的项目的_id
+    if (await isblocked()) {
+        console.log("ip被封，请检查")
+        setTimeout(dispatch, 1000 * 60 * 30, crawler)
+        return
+    }
     let index = await crawler.getId()
     console.log(index)
-    if (index === 'nil' || index === null) { process.exit(1) }
+    if (index === 'nil' || index === null) { crawlAllCompleted("rank-redis") }
     await crawler.crawl(index)
         .then(async function(ret) {
             if (await crawler.save(ret)) {
@@ -72,23 +73,27 @@ function usage() {
   `)
 }
 
-async function run(name, inc) {
+async function run(name, inc = false) {
     if (!name) { return usage() }
     let [file, mod] = name.split('-')
     let crawler = require(`./douban/${file}`)[mod || 'default']
     if (inc) {
-        console.log("重头开始")
-        try {
-            let films = await crawler.getIds()
-            let redisRes = await crawler.saveRedis(films)
-        } catch (e) {
-            console.log(e)
-        }
+        console.log("重头开始,读取film里的剧目放到redis", new Date())
+        var j = schedule.scheduleJob('* 8 * * *', function() {
+            console.log('每天8点开始执行');
+            try {
+                let films = await crawler.getIds()
+                let redisRes = await crawler.saveRedis(films)
+            } catch (e) {
+                console.log(e)
+            }
+        });
     } else {
         console.log("从上次开始")
+        dispatch(crawler)
     }
 
-    dispatch(crawler)
+
 }
 filmMongo.then(x => {
     global.filmMongo = x
