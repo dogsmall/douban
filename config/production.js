543594c27147ee31a0604a3bea4b9414437f9f86
guoshiwei <guoshiwei@iMac-2.local>
2017-06-13 11:12:55 +0800

    gsw

--- config/production.js
+++ config/production.js
@@ -1,20 +1,22 @@
-// export const ID_PER = 50
-// export const MIN_QUESTION_ID = 19550225
-// export const MAX_QUESTION_ID = 60000000
-// export const MIN_TOPIC_ID = 19550225
-// export const MAX_TOPIC_ID = 20150000
-// export const beattime = 1000 * 30
-// export const cleartime = 1000 * 60 * 10
-// export const redisOpts = {
-//     host: '101.201.37.28'
-//   , port: '6379'
-//   , password: 'Abc123456'
-// }
+export const ID_PER = 50
+export const MIN_QUESTION_ID = 19550225
+export const MAX_QUESTION_ID = 60000000
+export const MIN_TOPIC_ID = 19550225
+export const MAX_TOPIC_ID = 20150000
+export const beattime = 1000 * 30
+export const cleartime = 1000 * 60 * 10
+export const redisOpts = {
+    host: '101.201.37.28',
+    port: '6379',
+    password: 'Abc123456'
+}
 
 // export const esOpts = {
 //     hosts: ["http://idatage:abc@123456@59.110.52.213/port/"]
-//   // , auth: 'idatage:abc@123456'
-//   , requestTimeout: 300000
-//   , log: 'error'
+//         // , auth: 'idatage:abc@123456'
+//         ,
+//     requestTimeout: 300000,
+//     log: 'error'
 // }
-// export const mongoUrl = "mongodb://zhihu:joke123098@101.201.37.28:3717/zhihu"
+export const mongoUrl = "mongodb://douban:joke123098@101.201.37.28:3717/douban"
+export const filmMongoUrl = "mongodb://normal:Joke123@ant09.idatage.com:27021/tarantula"
